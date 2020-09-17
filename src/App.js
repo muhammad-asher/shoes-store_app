2239f371ab1a811cbc8cfaf4eb414203e2478cd8
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2020-09-17 23:08:59 +0500

    Bringing some assets components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./Screens/Header/Header";
 
 function App() {
-	return <div className="App"><h1>Hello World</h1></div>;
+	return (
+		<div className="App">
+			<h1>Hello World</h1>
+		</div>
+	);
 }
 
 export default App;
