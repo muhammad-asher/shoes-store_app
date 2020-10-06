55df4a70ac1442983fdc4ca395aff1f6de25dd8b
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2020-10-06 11:37:47 +0500

    Fixed some issues

--- src/screens/Header/Header.js
+++ src/screens/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-import ShoeImg from '../../assets/hero.png';
+import ShoeImg from '../../assets/shoe.png';
 import BrandLogo from '../../assets/nike.png';
 
 import './Header.css';
